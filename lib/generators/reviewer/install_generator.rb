851dfa05cbe9fde73a68221c073deb90a58b77fd
cderche <cyrille@imac-de-gerard.home>
2017-04-04 18:02:40 +0200

    Fix for generator

--- lib/generators/reviewer/install_generator.rb
+++ lib/generators/reviewer/install_generator.rb
@@ -2,15 +2,14 @@ require 'rails/generators'
 require 'rails/generators/migration'
 require 'rails/generators/active_record'
 
-module ActiveRecord
-  module Generators
-    class ReviewerGenerator < ActiveRecord::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
+module Reviewer
+    class InstallGenerator < ::Rails::Generators::Base
+        include ::Rails::Generators::Migration
 
-      def create_migration_file
-        migration_template 'migration.rb', 'db/migrate/create_reviews.rb'
-      end
+        source_root File.expand_path('../templates', __FILE__)
 
+        def create_migration_file
+            migration_template 'migration.rb', 'db/migrate/create_reviews.rb'
+        end
     end
-  end
 end
